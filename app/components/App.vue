d673dbdd1876829d13f7eb20f53f31fa66822ad3
Abdiel Martinez <aabdiel1998@gmail.com>
2020-03-04 13:47:44 -0600

    initial commit

---
+++ app/components/App.vue
@@ -0,0 +1,76 @@
+<template lang="html">
+    <BottomNavigation>
+        <TabStrip>
+            <TabStripItem class="navigation__item">
+                <Label text="Home"></Label>
+                <Image src.decode="font://&#xf015;" class="fas t-36"></Image>
+            </TabStripItem>
+            <TabStripItem class="navigation__item">
+                <Label text="Browse"></Label>
+                <Image src.decode="font://&#xf1ea;" class="far t-36"></Image>
+            </TabStripItem>
+            <TabStripItem class="navigation__item">
+                <Label text="Search"></Label>
+                <Image src.decode="font://&#xf002;" class="fas t-36"></Image>
+            </TabStripItem>
+        </TabStrip>
+
+        <TabContentItem>
+            <Frame>
+                <Items />
+            </Frame>
+        </TabContentItem>
+
+        <TabContentItem>
+            <Frame>
+                <Browse />
+            </Frame>
+        </TabContentItem>
+
+        <TabContentItem>
+            <Frame>
+                <Search />
+            </Frame>
+        </TabContentItem>
+
+    </BottomNavigation>
+</template>
+
+<script>
+import Items from "./Items.vue";
+import Browse from "./Browse.vue";
+import Search from "./Search.vue";
+const camera = require("nativescript-camera");
+
+export default {
+  components: {
+    Items,
+    Browse,
+    Search
+  },
+  mounted(){
+	  camera.requestPermissions()
+	  .then(
+    function success() {
+		alert('tengo permiso')
+	    // permission request accepted or already granted 
+        // ... call camera.takePicture here ...
+    }, 
+    function failure() {
+		alert('no tengo permiso')
+        // permission request rejected
+        // ... tell the user ...
+    }
+);
+  }
+};
+</script>
+
+<style lang="scss">
+// Start custom common variables
+@import "~@nativescript/theme/scss/variables/blue";
+// End custom common variables
+
+// Custom styles
+
+</style>
